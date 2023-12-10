9cf82adb38724282c5a354f1f8cc383fa0677541
Bert Ramlot <bert.ramlot@supportsquare.io>
2023-12-10 11:38:16 +0100

    test cleanup

--- tests/card_set_test.rs
+++ tests/card_set_test.rs
@@ -21,7 +21,7 @@ mod cardset_test {
             for j in i+1..card_count {
                 card_set.cards[j].0 = card_set.cards[i].0 + (j - i) as u8;
             }
-            break;            
+            break;
         }
 		return true;
 	}
@@ -31,96 +31,82 @@ mod cardset_test {
 
 	#[test]
 	fn test_count_different_isomorphic_hands() {
-
-
         for card_count in 1..EXPECTED_ISOMORPHIC_HAND_COUNT.len() {
 			let mut hand_count = 0u64;
-            // println!("Starting card_count={}", card_count);
-            let mut card_vec = Vec::new();
-            for i in 0..card_count {
-                card_vec.push(i as u8);
-            }
-            let mut cards: CardSet = card_vec.into();
-            let mut seen_canonicals = HashSet::new();
+            let mut card_set: CardSet = (0..card_count as u8).collect::<Vec<u8>>().into();
+            let mut seen_identifiers = HashSet::new();
             loop {
-                let cannonical = cards.clone().as_canonical();
-                seen_canonicals.insert(cannonical.identifier());
 				hand_count += 1;
-                if !increment_cardset(&mut cards) {
+                let cannonical = card_set.clone().as_canonical();
+                seen_identifiers.insert(cannonical.identifier());
+                if !increment_cardset(&mut card_set) {
                     break;
                 }
             }
 			assert_eq!(
 				EXPECTED_HAND_COUNT[card_count],
 				hand_count, 
-				"Expected {} different generated card_sets with {} cards, got {}",
-				EXPECTED_HAND_COUNT[card_count],
-				card_count,
-				hand_count
+				"Incorrect amount of hands for CarSet with {} cards",
+				card_count
 			);
             assert_eq!(
 				EXPECTED_ISOMORPHIC_HAND_COUNT[card_count],
-				seen_canonicals.len() as u32,
-				"Expected {} different isomorphic card_sets with {} cards, got {}",
-				EXPECTED_ISOMORPHIC_HAND_COUNT[card_count],
-				card_count,
-				seen_canonicals.len()
+				seen_identifiers.len() as u32,
+				"Incorrect amount of isomorphic hands for CarSet with {} cards",
+				card_count
 			);
 		}
 	}
 
 	#[test]
 	fn test_canonical_eval() {
-		const EXPECTED_COUNTS: [(&str, u32); 9] = [
-			("St/Ro flush",        41584),
-			("Four of a kind",    224848),
-			("Full house",       3473184),
-			("Flush",            4047644),
-			("Straight",         6180020),
-			("Three of a kind",  6461620),
-			("Two pair",        31433400),
+		const EXPECTED_EVAL_COUNTS: [(&str, u32); 9] = [
+			("High card",       23294460),
 			("One pair",        58627800),
-			("High card",       23294460)
+			("Two pair",        31433400),
+			("Three of a kind",  6461620),
+			("Straight",         6180020),
+			("Flush",            4047644),
+			("Full house",       3473184),
+			("Four of a kind",    224848),
+			("St/Ro flush",        41584),
 		];
 
-		let mut input: CardSet = vec![0, 1, 2, 3, 4, 5, 6].into();
-		let mut eval_type_count = vec![0u32; 9];
+		let mut card_set: CardSet = (0..7u8).collect::<Vec<u8>>().into();
+		let mut eval_type_count = [0u32; 9];
 		let mut hand_count = 0u64;
 		loop {
-            let output = input.clone().as_canonical();
-            let eval = output.evaluate();
-			let eval_type_count_index = eval_type_count.len()-1 - ((eval >> 20) as usize);
-			eval_type_count[eval_type_count_index] += 1;
+            let cannonical = card_set.clone().as_canonical();
+            let eval_type = cannonical.evaluate() >> 20;
+			eval_type_count[eval_type as usize] += 1;
 			hand_count += 1;
-			if !increment_cardset(&mut input) {
+			if !increment_cardset(&mut card_set) {
 				break;
 			}
 		}
 
-		assert_eq!(EXPECTED_HAND_COUNT[7], hand_count,
-			"Expected {} different generated card_sets with {} cards, got {}", EXPECTED_HAND_COUNT[7], 7, hand_count);
+		assert_eq!(
+			EXPECTED_HAND_COUNT[7],
+			hand_count,
+			"Incorrect amount of hands for CarSet with 7 cards"
+		);
 
 		let mut success = true;
+		let mut err_string = format!("        {:15} {:8}    {:8}\n", "Hand type", "Expected", "Got");
 		for i in 0..eval_type_count.len() {
-			let (_s, c) = EXPECTED_COUNTS[i];
-			if c != eval_type_count[i] {
+			let (type_str, count) = EXPECTED_EVAL_COUNTS[i];
+			if count == eval_type_count[i] {
+				err_string.push_str(&format!("        {:15} {:8} == {:8}\n", type_str, count, eval_type_count[i]));
+			} else {
+				err_string.push_str(&format!("Wrong > {:15} {:8} != {:8}\n", type_str, count, eval_type_count[i]));
 				success = false;
-				break;
 			}
 		}
-
-		if !success {
-			let mut err_string = format!("\n \t{:15} {:8}    {:8}\n", "Hand type", "Expected", "Got");
-			for i in 0..eval_type_count.len() {
-				let (s, c) = EXPECTED_COUNTS[i];
-				if c == eval_type_count[i] {
-					err_string.push_str(&format!(" \t{:15}: {:8} == {:8}\n", s, c, eval_type_count[i]));
-				} else {
-					err_string.push_str(&format!(">\t{:15}: {:8} != {:8}\n", s, c, eval_type_count[i]));
-				}
-			}
-			panic!("{}", err_string);
-		}
+		assert!(
+			success,
+			"{}",
+			err_string
+		);
 	}
 
 	#[test]
@@ -128,37 +114,41 @@ mod cardset_test {
 		let path = Path::new("./test_resources/eval_order_test.txt");
 		let file = match File::open(path) {
 			Ok(f) => f,
-			Err(e) => panic!("Failed to open file: {}", e),
+			Err(e) => panic!("Failed to eval_order_test file: {}", e),
 		};
 		let reader = BufReader::new(file);
 	
-		let mut last_comment = "???".to_owned();
-		let mut prev_str = "None".to_owned();
-		let mut prev_eval = 1;
+		let mut section = "".to_owned();
+		let mut prev_str = "".to_owned();
+		let mut prev_eval = -1;
 		for (i, line) in reader.lines().enumerate() {
             let curr_str = line.unwrap();
 			if curr_str.len() == 0 {
 				continue;
 			}
 			if curr_str.chars().nth(0).unwrap() == '#' {
-				last_comment = (&curr_str[2..]).to_owned();
+				section = curr_str[2..].to_owned();
 				continue;
 			}
-			let cards: CardSet = (&curr_str[2..]).into();
-			let canonical_cards = cards.as_canonical();
-			let curr_eval = canonical_cards.evaluate();
-			if prev_str != "None" {
+			let cards: CardSet = curr_str[2..].into();
+			let curr_eval = cards.as_canonical().evaluate();
+
+			if prev_str != "" {
 				let success = match curr_str.chars().nth(0).unwrap() {
 					'>' => prev_eval > curr_eval,
 					'=' => prev_eval == curr_eval,
-					_ => panic!("Invalid eval order test file"),
+					'<' => prev_eval < curr_eval,
+					_ => panic!("Invalid comperator"),
 				};
 				assert!(
 					success,
-					"\n\t<Line={}, Section='{}'> Expected different order\n\t\tPrevious: '{}' -> Eval={}\n\t\tCurrent : '{}' -> Eval={}\n",
-					i+1, last_comment, prev_str, prev_eval, curr_str, curr_eval
+					"\n\t<Line={}, Section='{}'> Comparison does not hold:\n\t\tPrevious: '{}' -> Eval={}\n\t\tCurrent : '{}' -> Eval={}\n",
+					i+1, section,
+					prev_str, prev_eval,
+					curr_str, curr_eval
 				);
 			}
+
 			prev_eval = curr_eval;
 			prev_str = curr_str;
 		}
--- tests/game_test.rs
+++ tests/game_test.rs
@@ -3,6 +3,7 @@
 mod game_test {
     use poker::round_state::{RoundState, RoundStage};
 
+    // TODO: add more tests beside this complex case
     #[test]
     pub fn test_chip_distribution() {
         let mut round_state = RoundState {
@@ -20,19 +21,24 @@ mod game_test {
                 "6h 2d", // 7 3th
             ].iter().map(|&s| s.into()).collect(),
             bet_chips:  vec![3.0, 50.0,  6.0, 10.0, 15.0, 110.0, 15.0, 130.0],
-            free_chips: vec![0.0,  0.0, 25.0,  0.0,  0.0,  0.0,  10.0,  20.0],
+            free_chips: vec![0.0,  0.0, 25.0,  0.0,  0.0,   0.0, 10.0,  20.0],
             folded: (1 << 2) | (1 << 6),
             ..Default::default()
         };
         while !round_state.is_finished() {
             round_state.do_action(0.0);
         }
-        assert!(round_state.is_finished(), "rs1 should be finished");
+
+        assert_eq!(
+            round_state.bet_chips,
+            vec![0.0; 8],
+            "Bet chips should be removed when round is finished"
+        );
 
         assert_eq!(
             round_state.free_chips,
             vec![0.0, 105.0, 40.0, 27.0, 52.0, 0.0, 10.0, 160.0],
-            "Wrong distribution of chips"
+            "Chips distributed incorrectly"
         );
     }
 }
